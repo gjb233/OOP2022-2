eda32f39b8ed6bd3ab26115b67cb84460ad7ab94
gjb233 <guojh23@mails.tsinghua.edu.cn>
2024-06-21 10:40:00 +0800

    fourth change

--- Plane.h
+++ Plane.h
@@ -11,6 +11,6 @@ public:
         return (wheel_num >= max_wheel_num) && (wing_num >= max_wing_num);
     };
     void run() override{
-        cout << "I am flying and running" << endl;
+        cout << "I am running and flying" << endl;
     }
 };
