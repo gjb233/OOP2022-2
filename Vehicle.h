1e90d261d990eba457e59881b02e749c56ce04b3
gjb233 <guojh23@mails.tsinghua.edu.cn>
2024-06-21 10:06:32 +0800

    initial commit

---
+++ Vehicle.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Vehicle{
+public:
+    int wheel_num;
+    int wing_num;
+    int max_wheel_num;
+    int max_wing_num;
+    void set_max_wheel_num(int n){
+        max_wheel_num = n;
+    }
+    void set_max_wing_num(int n){
+        max_wing_num = n;
+    }
+    void add_wheel(){
+        wheel_num++;
+    }
+    void add_wing(){
+        wing_num++;
+    }
+    virtual bool finished(){
+        return wheel_num >= max_wheel_num;
+    };
+    virtual void run(){
+        cout << "I'm running" << endl;
+    };
+};
